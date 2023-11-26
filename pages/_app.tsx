a4e1d05f938a66b5a1dd6ccc910a514544acd0e1
Vitaly Rtishchev <rtivital@gmail.com>
2023-11-26 12:50:57 +0400

    Update code highlight to shiki

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,23 @@
 import '../fonts/GreycliffCF/styles.css';
 import '@mantine/core/styles.css';
-import '@mantine/code-highlight/styles.css';
+import '@mantinex/shiki/styles.css';
 import '@mantinex/demo/styles.css';
 import '@mantinex/mantine-logo/styles.css';
 import '@mantinex/mantine-header/styles.css';
 import Head from 'next/head';
 import { MantineProvider } from '@mantine/core';
+import { ShikiProvider } from '@mantinex/shiki';
 import { Shell } from '../components/Shell';
 import { theme } from '../theme';
+
+async function loadShiki() {
+  const { getHighlighter } = await import('shikiji');
+  const shiki = await getHighlighter({
+    langs: ['tsx', 'scss', 'html', 'bash', 'json'],
+  });
+
+  return shiki;
+}
 
 export default function App({ Component, pageProps }: any) {
   return (
@@ -21,7 +31,9 @@ export default function App({ Component, pageProps }: any) {
         <link rel="shortcut icon" href="/favicon.svg" />
       </Head>
       <Shell>
-        <Component {...pageProps} />
+        <ShikiProvider loadShiki={loadShiki}>
+          <Component {...pageProps} />
+        </ShikiProvider>
       </Shell>
     </MantineProvider>
   );
