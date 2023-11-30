e034f84a31942b6b339b971e8412a7d9dde20bae
Vitaly Rtishchev <rtivital@gmail.com>
2023-11-30 11:52:49 +0400

    Add disable all inputs question

---
+++ demos/FieldsetInputs.demo.tsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import { MantineDemo } from '@mantinex/demo';
+import { Fieldset, TextInput, Button } from '@mantine/core';
+
+const code = `
+import { Fieldset, TextInput, Button } from '@mantine/core';
+
+function Demo() {
+  return (
+    <form>
+      <Fieldset disabled variant="unstyled">
+        <TextInput label="Your name" placeholder="Your name" />
+        <TextInput label="Your email" placeholder="Your email" mt="md" />
+        <Button type="submit" mt="md">
+          Submit
+        </Button>
+      </Fieldset>
+    </form>
+  );
+}
+`;
+
+function Demo() {
+  return (
+    <form>
+      <Fieldset disabled variant="unstyled">
+        <TextInput label="Your name" placeholder="Your name" />
+        <TextInput label="Your email" placeholder="Your email" mt="md" />
+        <Button type="submit" mt="md">
+          Submit
+        </Button>
+      </Fieldset>
+    </form>
+  );
+}
+
+export const FieldsetInputs: MantineDemo = {
+  type: 'code',
+  component: Demo,
+  code,
+};
