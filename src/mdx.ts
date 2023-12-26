faba26388c74200e77f456fa89862011bd813664
Vitaly Rtishchev <rtivital@gmail.com>
2023-12-26 19:12:47 +0400

    Prevent prettier imports sorting in questions list

--- src/mdx.ts
+++ src/mdx.ts
@@ -1,8 +1,8 @@
+import { meta as q1 } from './pages/q/how-to-update-dependencies.mdx';
 import { meta as q2 } from './pages/q/can-i-use-mantine-with-cra.mdx';
 import { meta as q3 } from './pages/q/disable-all-inputs-in-form.mdx';
+import { meta as q4 } from './pages/q/how-to-use-dropzone-with-form.mdx';
 import { meta as q6 } from './pages/q/how-to-add-hover-styles.mdx';
 import { meta as q5 } from './pages/q/how-to-prevent-modal-from-closing.mdx';
-import { meta as q1 } from './pages/q/how-to-update-dependencies.mdx';
-import { meta as q4 } from './pages/q/how-to-use-dropzone-with-form.mdx';
 
 export const MDX_DATA = [q1, q2, q3, q4, q5, q6];
