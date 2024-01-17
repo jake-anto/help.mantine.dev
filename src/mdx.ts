7f38a1e8960419ba20c6570f30a5e5a12888fdee
Vitaly Rtishchev <rtivital@gmail.com>
2024-01-17 13:00:54 +0400

    Add floating action button question

--- src/mdx.ts
+++ src/mdx.ts
@@ -3,6 +3,7 @@ import { meta as canIUseMantineWithCra } from './pages/q/can-i-use-mantine-with-
 import { meta as dataGridINeed } from './pages/q/data-grid-i-need.mdx';
 import { meta as disableAllInputsInForm } from './pages/q/disable-all-inputs-in-form.mdx';
 import { meta as dynamicCssStyles } from './pages/q/dynamic-css-styles.mdx';
+import { meta as floatingActionButton } from './pages/q/floating-action-button.mdx';
 import { meta as howCanIContribute } from './pages/q/how-can-i-contribute.mdx';
 import { meta as howToAddHoverStyles } from './pages/q/how-to-add-hover-styles.mdx';
 import { meta as howToCallFunctionWhenModalCloses } from './pages/q/how-to-call-function-when-modal-closes.mdx';
@@ -24,6 +25,7 @@ export const MDX_DATA = [
   dataGridINeed,
   disableAllInputsInForm,
   dynamicCssStyles,
+  floatingActionButton,
   howCanIContribute,
   howToAddHoverStyles,
   howToCallFunctionWhenModalCloses,
